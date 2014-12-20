d15f0da315bd237428369d43a0768aa8adc2fc60
Atushi Ando <andotrue@gmail.com>
2014-12-20 17:36:30 +0900

    @homemac フロントページに新着情報を表示

---
+++ wp-content/themes/twentyfourteen/front-page.php
@@ -0,0 +1,69 @@
+<?php
+get_header(); ?>
+
+
+<div id="main-content" class="main-content">
+
+<?php
+	if ( is_front_page() && twentyfourteen_has_featured_posts() ) {
+		// Include the featured content template.
+		get_template_part( 'featured-content' );
+	}
+?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+
+		<?php
+			if ( have_posts() ) :
+				// Start the Loop.
+				while ( have_posts() ) : the_post();
+
+					/*
+					 * Include the post format-specific template for the content. If you want to
+					 * use this in a child theme, then include a file called called content-___.php
+					 * (where ___ is the post format) and that will be used instead.
+					 */
+					get_template_part( 'content', get_post_format() );
+
+				endwhile;
+				// Previous/next post navigation.
+				twentyfourteen_paging_nav();
+
+			else :
+				// If no content, include the "No posts found" template.
+				get_template_part( 'content', 'none' );
+
+			endif;
+		?>
+
+<section id="news" class="page-content">
+	<h1>最新情報</h1>
+<ul>
+	<?php 
+		global $post;
+		$args = array( 'posts_per_page' => 5 );
+		$myposts = get_posts($args);
+		foreach( $myposts as $post ) {
+			setup_postdata($post);
+	?>
+	<li>
+<time datetime="<?php the_time('c'); ?>"><?php the_time('Y.m.d') ?></time>
+<span class="category"><?php the_category('|') ?></span>
+<a href="<?php the_permlink() ?>"><?php the_title(); ?></a>
+	</li>
+	<?php 
+		}
+		wp_reset_postdata();
+	?>
+</ul>
+</section>		
+		
+		</div><!-- #content -->
+	</div><!-- #primary -->
+	<?php get_sidebar( 'content' ); ?>
+</div><!-- #main-content -->
+
+<?php
+get_sidebar();
+get_footer();
